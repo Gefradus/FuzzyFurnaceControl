60b56b4583ed95e4e4501cf29af20ad4f9f6508a
dratwak <krzysztof.dratwa@ttsw.com.pl>
2021-01-11 03:34:31 +0100

    prettier 4

--- src/main/java/RealTimeChart.java
+++ src/main/java/RealTimeChart.java
@@ -9,10 +9,38 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-public class RealTimeChart extends Stage {
+public class RealTimeChart extends Stage
+{
+    private XYChart.Series<String, Number> series;
 
     public RealTimeChart(double[] getTempOrPower, String chartTitle, int sleepMillisecond)
     {
+        setScene(new Scene(createInstanceOfChart(chartTitle), 800, 600));
+        setTitle(chartTitle);
+        show();
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
+        Thread updateThread = new Thread(() -> {
+            try
+            {
+                Calendar cal = Calendar.getInstance();
+                cal.clear();
+                for(int i = 0; i <= 1439; i++){
+                    cal.add(Calendar.MINUTE, 1);
+                    Thread.sleep(sleepMillisecond);
+                    Date time = cal.getTime();
+                    int finalI = i;
+                    Platform.runLater(() -> series.getData().add(new XYChart.Data<>(simpleDateFormat.format(time), getTempOrPower[finalI])));
+                }
+            }
+            catch (Exception ignored) {}
+        });
+
+        updateThread.setDaemon(true);
+        updateThread.start();
+    }
+
+    private LineChart<String, Number> createInstanceOfChart(String chartTitle){
         final CategoryAxis xAxis = new CategoryAxis();
         final NumberAxis yAxis = new NumberAxis();
 
@@ -20,17 +48,15 @@ public class RealTimeChart extends Stage {
         xAxis.setLabel("Czas");
         yAxis.setAnimated(false);
 
-        XYChart.Series<String, Number> series = new XYChart.Series<>();
+        series = new XYChart.Series<>();
 
         if (chartTitle.equals("Moc pieca w czasie")){
             yAxis.setLabel ("P [kW]");
             series.setName("Moc pieca");
-
         }
         else if (chartTitle.equals("Temperatury zewnętrzne w czasie")){
             yAxis.setLabel ("T [°C]");
             series.setName("Temperatury zewnętrzne");
-
         }
         else {
             yAxis.setLabel("T [°C]");
@@ -40,31 +66,8 @@ public class RealTimeChart extends Stage {
         LineChart<String, Number> chart = new LineChart<>(xAxis, yAxis);
         chart.setAnimated(false);
         chart.getData().add(series);
-
-        Scene scene = new Scene(chart, 800, 600);
-        setScene(scene);
-        show();
-        setTitle(chartTitle);
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
-        Thread updateThread = new Thread(() -> {
-            try
-            {
-                Calendar cal = Calendar.getInstance();
-                cal.clear();
-                for(int j=0; j<=1439; j++){
-                    cal.add(Calendar.MINUTE, 1);
-                    Thread.sleep(sleepMillisecond);
-                    Date time = cal.getTime();
-                    int finalI = j;
-                    Platform.runLater(() -> series.getData().add(new XYChart.Data<>(simpleDateFormat.format(time), getTempOrPower[finalI])));
-                }
-            }
-            catch (Exception ignored) {}
-
-        });
-
-        updateThread.setDaemon(true);
-        updateThread.start();
+        return chart;
     }
+
 
 }
